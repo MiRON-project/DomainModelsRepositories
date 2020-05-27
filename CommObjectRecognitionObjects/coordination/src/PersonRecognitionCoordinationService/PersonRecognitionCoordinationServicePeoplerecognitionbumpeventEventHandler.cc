21b301bc5d9964f4de563058ad84309608c95736
renan028 <renan028@gmail.com>
2020-05-27 00:01:48 -0300

    add person bump

---
+++ CommObjectRecognitionObjects/coordination/src/PersonRecognitionCoordinationService/PersonRecognitionCoordinationServicePeoplerecognitionbumpeventEventHandler.cc
@@ -0,0 +1,30 @@
+#include "PersonRecognitionCoordinationServicePeoplerecognitionbumpeventEventHandler.hh"
+
+std::string PersonRecognitionCoordinationServicePeoplerecognitionbumpeventEventHandler::handleEvent(const CommObjectRecognitionObjects::CommObjectRecognitionEventBumpResult &r) throw() {
+	std::string outString;
+	
+	const int envId = r.getEnvironment_id();
+	std::ostringstream eventState;
+
+	eventState << "(" << envId;
+	eventState << "(";
+
+	for (auto& id : r.getObject_idCopy())
+		eventState << " " << id;
+	eventState << "))";
+
+	//convert the event result commObject to string representation outString (to TCL)
+	//std::ostringstream ss;
+	//ss << "("<< std::setprecision( 2 ) << answer_data<<")";
+	//outString = ss.str();
+	outString = eventState.str();
+	return outString;
+}
+
+CommBasicObjects::CommVoid PersonRecognitionCoordinationServicePeoplerecognitionbumpeventEventHandler::activateEventParam(const std::string& parameterString){
+	CommBasicObjects::CommVoid param;
+	
+	//fill the event activation (parameter) commObject with the data provided via the inString (from TCL)
+	//e.g. param.setLisp(inString);
+	return param;
+}
